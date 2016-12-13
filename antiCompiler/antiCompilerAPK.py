e02ecf3f24e4c8a2cfec30ee636d7e30ad424c7b
gaoqinghong <1134639599@qq.com>
2016-12-13 09:04:36 +0800

    已经把每个apk文件的路径保存到了filepathlist中

--- antiCompiler/antiCompilerAPK.py
+++ antiCompiler/antiCompilerAPK.py
@@ -7,22 +7,37 @@ filepath = '/Users/gaoqinghong/Downloads/homework/MachineLearning/apk'
 filelist = os.listdir(filepath)
 
 listAllApkFile = []      #存储所有apk文件名
+listfilepath = []         #存储bad，good1，good2文件夹的路径
 i = 0
 for list in filelist:    #读取所有apk文件名，存储到list内
     if(i == 0):
         i += 1
         continue
     filepathnow = filepath+'/'+list
+    listfilepath.append(filepathnow)
     listAllApkFile = listAllApkFile+os.listdir(filepathnow)
+    #print len(listAllApkFile)
+
+filepathlist = []
+for list in range(1000):
+    filepathlist.append(listfilepath[0]+'/'+listAllApkFile[list])
+
+for list in range(499):
+    filepathlist.append(listfilepath[1]+'/'+listAllApkFile[1000+list])
+
+for list in range(500):
+    filepathlist.append(listfilepath[2] + listAllApkFile[1499+list])
+
+print len(filepathlist)
 
 
 apkclass = []            # 为每个apk文件创建一个对象
-for i in range(len(listAllApkFile)):
-    apkclass[i] = apk.APK(filepath + "/" + "bad" + "/" + listAllApkFile[1], raw=False, mode='r', magic_file=None, zipmodule=1)
+for i in range(len(filepathlist)):
+    apkclass.append(apk.APK(filepathlist[i], raw=False))
 
-permission = apk.get_permissions()
-for list in permission:
-    print list
+permissions = []
+for list in range(len(apkclass)):
+   permissions[list] = apkclass[list]
 
 
 
